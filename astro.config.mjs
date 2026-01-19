7ae7344d4c3e81485037a69e04e97225a85b61dd
zachschuler <Zrschu@gmail.com>
2026-01-19 00:13:53 -0500

    uncommenting astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-// // @ts-check
-// import { defineConfig } from 'astro/config';
+// @ts-check
+import { defineConfig } from 'astro/config';
 
-// import cloudflare from '@astrojs/cloudflare';
+import cloudflare from '@astrojs/cloudflare';
 
-// // https://astro.build/config
-// export default defineConfig({
-//   site: 'https://southpittrugby.com',
-//   adapter: cloudflare()
-// });
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://southpittrugby.com',
+  adapter: cloudflare()
+});
